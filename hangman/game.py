34dbb42348e439374887a6f22aa7c8a7e105e927
gmrkss <parlamasgeorge@gmail.com>
2019-05-07 01:38:49 +0000

    Passing more tests hangman_game.py

--- hangman/game.py
+++ hangman/game.py
@@ -2,7 +2,7 @@ from .exceptions import *
 import random
 
 class GuessAttempt(object):
-    def __init__(self, attempt, hit=None, miss=None):
+    def __init__(self, attempt, hit=False, miss=False):
         self.attempt = attempt
         self.hit = hit
         self.miss = miss
@@ -20,10 +20,9 @@ class GuessAttempt(object):
         return False
 
 class GuessWord(object):
-    def __init__(self, answer, masked=None, attempt=None):
+    def __init__(self, answer):
         self.answer = answer.lower()
         self.masked = len(self.answer)*"*"
-        self.attempt = GuessAttempt(attempt, hit=None, miss=None)
         if self.answer=='':
             raise InvalidWordException
 
@@ -46,48 +45,47 @@ class GuessWord(object):
 
             self.masked=''.join(masked_word_chars)    
             
-            setattr(self.attempt, 'hit', True)
-            setattr(self.attempt, 'miss', False)
-            
-            return self.attempt
-        
+            return GuessAttempt(attempt, hit=True)
+
         else:
-            setattr(self.attempt, 'hit', False)
-            setattr(self.attempt, 'miss', True)
-            
-            return self.attempt
+            return GuessAttempt(attempt, miss=True)
 
 class HangmanGame(object):
-    def __init__(self, list_of_words, number_of_guesses=5, remaining_misses=5):
-        self.list_of_words = list_of_words
-        self.number_of_guesses = number_of_guesses
+    def __init__(self, list_of_words=['rmotr', 'python', 'awesome'], number_of_guesses=5):
         self.remaining_misses = number_of_guesses
+        self.word = GuessWord(HangmanGame.select_random_word(list_of_words))
+        self.previous_guesses=[]
+        self.WORD_LIST = list_of_words
     
-    def select_random_word(self):
-        if self == []:
+    @classmethod
+    def select_random_word(cls, list_of_words):
+        if list_of_words == []:
             raise InvalidListOfWordsException
-        return random.choice(self)
-        
-    def start_new_game(self):
-        select_random_word(self)
-        if self == []:
-            raise InvalidListOfWordsException
+        return random.choice(list_of_words)
+
+    
+    
+    
+#     def start_new_game(self):
+#         select_random_word(self)
+#         if self == []:
+#             raise InvalidListOfWordsException
         
         
         
     
     
-    def start_new_game(list_of_words=None, number_of_guesses=5):
-    if list_of_words is None:
-        list_of_words = LIST_OF_WORDS
+#     def start_new_game(list_of_words=None, number_of_guesses=5):
+#     if list_of_words is None:
+#         list_of_words = LIST_OF_WORDS
 
-    word_to_guess = _get_random_word(list_of_words)
-    masked_word = _mask_word(word_to_guess)
-    game = {
-        'answer_word': word_to_guess,
-        'masked_word': masked_word,
-        'previous_guesses': [],
-        'remaining_misses': number_of_guesses,
-    }
+#     word_to_guess = _get_random_word(list_of_words)
+#     masked_word = _mask_word(word_to_guess)
+#     game = {
+#         'answer_word': word_to_guess,
+#         'masked_word': masked_word,
+#         'previous_guesses': [],
+#         'remaining_misses': number_of_guesses,
+#     }
 
-    return game
+#     return game
